413076f5b2b7dc50cd5b84c3ee0f84808352c681
Angel Legorreta <cdmx1895@amerike.edu>
2024-12-10 18:26:11 -0600

    First Version of Spawner

--- Proyect Water Faucet/Assets/AlmejaWork/Code/V2/DripingSystem/DropSpawner.cs
+++ Proyect Water Faucet/Assets/AlmejaWork/Code/V2/DripingSystem/DropSpawner.cs
@@ -1,20 +1,59 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using Lean.Pool;
 using Unity.Mathematics;
 
 public class DropSpawner : MonoBehaviour
 {
+    #region Variables
+
+    [Header("Dripping Settings")]
+    [SerializeField] private GameObject dropPrefab;
+    [SerializeField] private float dropPerSecond;
+    [SerializeField] private Transform spawnPont;
+    [SerializeField] private float fallDistance;
+    [SerializeField] private SOBoolean isPaused;
+    private float dropInterval;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #endregion
+
+
+
+    private void Start()
     {
-        
+        dropInterval = 1f / dropPerSecond;
+
+        StartCoroutine(Drip());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Drip()
     {
+        while (true)
+        {
+            SpawnDrop();
+
+            yield return new WaitForSeconds(dropInterval);
+        }
+    }
+
+    private void SpawnDrop()
+    {
+        if (dropPrefab != null && spawnPont != null)
+        {
+            GameObject drop = LeanPool.Spawn(dropPrefab, spawnPont.position, Quaternion.identity);
+            DestroyDrop(drop);
+        }
+    }
+
+    private void DestroyDrop(GameObject drop)
+    {
+        float fallTime = math.sqrt(2 * fallDistance / Physics.gravity.magnitude);
         
+        LeanPool.Despawn(drop, fallTime);
+    }
+    public void SetDropPerSecond(float newRate)
+    {
+        dropPerSecond = Mathf.Max(0.1f, newRate);
+        dropInterval = 1f / dropPerSecond;
     }
 }
